8d5b4936a6737a0aa8ee689b9de0de8822a91663
unknown <fayzamashal94@gmail.com>
2024-05-16 14:13:39 +0300

    js-task1

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var products = document.querySelectorAll(".products .component")
+var div1 = document.querySelector("#div1")
+var btn1 = document.querySelector("#btn1")
+var div2 = document.querySelector("#div2")
+var Total = 0;
+
+products.forEach(function(item){
+    item.onclick = function(){
+        div1.innerHTML += item.textContent + "<br><br>"
+        if(div1.innerHTML != ""){
+            btn1.style.display = "block";
+            btn1.style.backgroundColor = "blue";
+            btn1.style.color = "white";
+            btn1.style.border = "none";
+            btn1.style.width = "100px";
+            btn1.style.height = "40px";
+            btn1.style.margin = "10px"
+            
+        }
+        Total += +(item.getAttribute("price"));
+    }
+})
+
+btn1.onclick = function(){
+    div2.innerHTML = Total
+
+}
